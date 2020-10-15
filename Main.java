1c59cbebaddb5aefe3c32642f16a8091ccfd4306
dlyons5 <dominic.lyons@patelhs.org>
2020-10-15 16:36:53 +0000

    fixed the scanner and boolean

--- Main.java
+++ Main.java
@@ -1,25 +1,28 @@
+import java.util.Scanner;
+
 class Main {
   
   public static void main(String[] args) {
 
-      Scanner scan = new Scanner(System.in); 
+    Scanner scan = new Scanner(System.in); 
     
-      System.out.println("choose a number between 1 and 5");
+    System.out.println("choose a number between 1 and 5");
     int number = scan.nextInt();
   
-      System.out.println("choose a number that adds to equal 12 (so like what + the number you choose = 12");
+    System.out.println("choose a number that adds to equal 12 (so like what + the number you choose = 12");
     int number2 = scan.nextInt();
     //now just make loops 
 
- boolean isCorrectMatch = false;
-     if (isCorrectMatch)
+ boolean isCorrectAnswer =true;
+     if (isCorrectAnswer)
      {
          System.out.println(" correct ");
      }
      else
      {
-         System.out.println(" thats the wrong match ");
+         System.out.println(" thats the wrong answer ");
      }
    }
 }
 
+int isCorrectAnswer = 
