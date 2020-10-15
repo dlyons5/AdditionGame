57665e2102b9ff46dba451a3f5870959d3deb922
dlyons5 <dominic.lyons@patelhs.org>
2020-10-15 16:18:21 +0000

    copy and pasted boolean

--- Main.java
+++ Main.java
@@ -2,14 +2,24 @@ class Main {
   
   public static void main(String[] args) {
 
-    Scanner scan = new Scanner(System.in); 
+      Scanner scan = new Scanner(System.in); 
     
-    System.out.println("choose a number between 1 and 5");
+      System.out.println("choose a number between 1 and 5");
     int number = scan.nextInt();
   
-    System.out.println("choose a number that adds to equal 12 (so like what + the number you choose = 12");
+      System.out.println("choose a number that adds to equal 12 (so like what + the number you choose = 12");
     int number2 = scan.nextInt();
     //now just make loops 
 
-  }
+ boolean isCorrectMatch = false;
+     if (isCorrectMatch)
+     {
+         System.out.println(" correct ");
+     }
+     else
+     {
+         System.out.println(" thats the wrong match ");
+     }
+   }
 }
+
