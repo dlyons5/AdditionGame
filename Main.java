8b327059aa2a81e9a52725b5b632412a3f248393
dlyons5 <dominic.lyons@patelhs.org>
2020-10-16 20:30:39 +0000

    added more comments

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@ class Main {
   public static void main(String[] args) {
  
    Scanner scan = new Scanner(System.in);
-  
-   System.out.println("choose a number between 1 and 5");
+//scanner is going to ask two questions   
+    System.out.println("choose a number between 1 and 5");
    int number = scan.nextInt();
     System.out.println("choose a number that adds to equal 12 (so like what + the number you choose = 12");
    int number2 = scan.nextInt();
- 
-while(correctAnswer >= 4){
-    //if the answer is less than 4 then
+//scanner is going to keep the answers of those questions 
+  while(correctAnswer >= 1 && <= 5 ){
+//if the answer is greater than or equal to 1 and less than or equal to 5
      System.out.println("The answer" + correctAnswer + "is correct because 8 + 4 = 12");
      correctAnswer++;
      }
     
      System.out.print("That is why the answer is 4");
-
+//int will decide if the answer is correct or not 
 int a = 1;
 int b = 2;
 int c = 3;
@@ -31,8 +31,7 @@ if (a <= 5 && e > 1)
     else
     {
         System.out.println(" thats the wrong answer ");
-      
-      
+//if the answer is correct, the code will print out correct, if the answer is wrong, the code will print out incorrect       
     }
   }
 }
