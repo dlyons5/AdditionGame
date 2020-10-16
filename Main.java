eb49fdda82e28b8ecb33f3c9de13e499d5466443
dlyons5 <dominic.lyons@patelhs.org>
2020-10-16 20:19:02 +0000

    changed the int variable and position of the while loop

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- 
+ //this allows the scanner to scan 
 class Main {
   public static void main(String[] args) {
  
@@ -9,25 +9,30 @@ class Main {
    int number = scan.nextInt();
     System.out.println("choose a number that adds to equal 12 (so like what + the number you choose = 12");
    int number2 = scan.nextInt();
-   //now just make loops
  
-int correctAnswer = 5;
-    if (1 < 5 && 8 + 4 == 12)
+while(correctAnswer >= 4){
+    //if the answer is less than 4 then
+     System.out.println("The answer" + correctAnswer + "is correct because 8 + 4 = 12");
+     correctAnswer++;
+     }
+    
+     System.out.print("That is why the answer is 4");
+
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+
+if (a <= 5 && e > 1)
     {
         System.out.println(" correct ");
     }
     else
     {
         System.out.println(" thats the wrong answer ");
-    
-while(correctAnswer <= 5){
-     //if the answer is less than 4 then
-      System.out.println("The answer" + correctAnswer + "is correct because 8 + 4 = 12");
-      correctAnswer++; 
-      }
       
-      System.out.print("That is why the answer is 4");
-    
+      
     }
   }
 }
